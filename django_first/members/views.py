7d0773dfb1b574be65bdb267fa60df7944d41d5e
Devyanshi Srivastava <devyanshi2106.com>
2022-11-13 23:27:46 +0530

    Added Records

--- django_first/members/views.py
+++ django_first/members/views.py
@@ -1,10 +1,23 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from django.http import HttpResponse
+from django.urls import reverse
 from .models import Members
 
 def index(request):
-    mymembers = Members.objects.all().values()
-    output = ""
-    for x in mymembers:
-        output += x["firstName"]
-    return HttpResponse(output)
+  mymembers = Members.objects.all().values()
+  template = loader.get_template('index.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+  
+def add(request):
+  template = loader.get_template('add.html')
+  return HttpResponse(template.render({}, request))
+  
+def addrecord(request):
+  x = request.POST.get['first',False]
+  y = request.POST.get['last',False]
+  member = Members(firstname=x, lastname=y)
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
