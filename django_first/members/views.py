69dcd69f8ad77a8b79567a7b4c6426173e8d5737
Devyanshi Srivastava <devyanshi2106@gmail.com>
2022-11-14 23:24:45 +0530

    Deleted and Updated Records

--- django_first/members/views.py
+++ django_first/members/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+
 from .models import Members
 
 def index(request):
@@ -16,8 +17,30 @@ def add(request):
   return HttpResponse(template.render({}, request))
   
 def addrecord(request):
-  x = request.POST.get['first',False]
-  y = request.POST.get['last',False]
-  member = Members(firstname=x, lastname=y)
+  x = request.POST.get('firstName')
+  y = request.POST.get('lastName')
+  member = Members(firstName=x, lastName=y)
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
+
+def delete(request, id):
+  member = Members.objects.get(id=id)
+  member.delete()
+  return HttpResponseRedirect(reverse('index'))
+
+def update(request, id):
+  mymember = Members.objects.get(id=id)
+  template = loader.get_template('update.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+def updaterecord(request, id):
+  first = request.POST['first']
+  last = request.POST['last']
+  member = Members.objects.get(id=id)
+  member.firstname = first
+  member.lastname = last
   member.save()
   return HttpResponseRedirect(reverse('index'))
